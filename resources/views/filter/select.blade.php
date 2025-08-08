f3f0b75fe6f6286308443da3991854ca51dc6fbc
Denis Astapenko <bobodrummer@gmail.com>
2025-08-08 11:32:41 +0200

    html cleaning

--- resources/views/filter/select.blade.php
+++ resources/views/filter/select.blade.php
@@ -1,6 +1,4 @@
 <select class="form-control {{ $class }}" name="{{$name}}" style="width: 100%;">
     <option></option>
-    @foreach($options as $select => $option)
-        <option value="{{$select}}" {{ (string)$select === (string)request($name, $value) ?'selected':'' }}>{{$option}}</option>
-    @endforeach
+    @foreach($options as $select => $option)<option value="{{$select}}" {{ (string)$select === (string)request($name, $value) ?'selected':'' }}>{{$option}}</option>@endforeach
 </select>
