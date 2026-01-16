1f627fa164aa26cd060e03064c3e66584dc26bd1
Denis Astapenko <bobodrummer@gmail.com>
2026-01-16 20:29:48 +0100

    fixed theme style

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
 
     <a href="{{ admin_url('/') }}" class="brand-link">
+        <span class="logo-mini">{!! config('admin.logo-mini', config('admin.name')) !!}</span>
         {{--        <img src="{!! config('admin.logo-mini-path', '/vendor/laravel-admin/AdminLTE/dist/img/AdminLTELogo.png') !!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">--}}
         <span class="brand-text font-weight-light">
             {!! config('admin.logo', config('admin.name')) !!}
