66f86043edcfacf68dc389f2404525e7ab05cb49
Denis Astapenko <bobodrummer@gmail.com>
2026-04-30 12:59:26 +0200

    added multiple for like filter

--- src/Grid/Filter/Like.php
+++ src/Grid/Filter/Like.php
@@ -17,6 +17,16 @@ class Like extends AbstractFilter
     protected $operator = 'like';
 
     /**
+     * @var string
+     */
+    public $separator = '||';
+
+    /**
+     * @var bool
+     */
+    public $multiple = false;
+
+    /**
      * Get condition of this filter.
      *
      * @param array $inputs
@@ -37,8 +47,34 @@ class Like extends AbstractFilter
 
         $this->value = $value;
 
-        $expr = str_replace('{value}', $this->value, $this->exprFormat);
+        if ($this->multiple) {
+            $values = array_filter(array_map('trim', explode($this->separator, $value)));
+            if (empty($values))
+                return;
 
-        return $this->buildCondition($this->column, $this->operator, $expr);
+            return $this->buildCondition(function ($query) use ($values) {
+                foreach ($values as $val) {
+                    $expr = str_replace('{value}', $val, $this->exprFormat);
+                    $query->orWhere($this->column, $this->operator, $expr);
+                }
+            });
+        } else {
+            $expr = str_replace('{value}', $this->value, $this->exprFormat);
+            return $this->buildCondition($this->column, $this->operator, $expr);
+        }
+    }
+
+    public function separator(string $separator): static
+    {
+        if ($separator !== '')
+            $this->separator = $separator;
+
+        return $this;
+    }
+
+    public function multiple(bool $multiple = true): static
+    {
+        $this->multiple = $multiple;
+        return $this;
     }
 }
